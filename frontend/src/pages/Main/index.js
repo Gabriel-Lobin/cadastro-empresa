9fadda691c38aab59a8d20d3fe042c19556565de
gabriel <gabrielsuassuna@hotmail.com>
2022-08-31 20:42:38 -0300

    feat: criação da pagina main

---
+++ frontend/src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CadastroInputEmpresa from "../../components/CadastroInputEmpresa";
+
+const Main = () => {
+    return (
+        <div>
+            <CadastroInputEmpresa />
+            Main
+        </div>
+    );
+}
+
+export default Main;
