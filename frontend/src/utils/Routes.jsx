91f5cc3a8e87a97e2ffc7403c12e24d9327cdf59
gabriel <gabrielsuassuna@hotmail.com>
2022-09-16 18:41:30 -0300

    feat: condicional de login para entrar na rota main e login

--- frontend/src/utils/Routes.jsx
+++ frontend/src/utils/Routes.jsx
@@ -18,8 +18,21 @@ function RoutesApp() {
                         : <Navigate to={loginPath} replace />
                 } />
 
-            <Route path={loginPath} element={<Login />} />
-            <Route path={mainPath} element={<Main />} />
+            <Route path={loginPath}
+                element={
+                    loggedIn
+                        ? <Navigate to={mainPath} replace />
+                        : <Login />
+
+                } />
+
+            <Route path={mainPath}
+                element={
+                    loggedIn
+                        ? <Main />
+                        : <Navigate to={loginPath} replace />
+                } />
+
             {/* <Route path={cadastroPath} /> */}
 
             <Route path="*" element={<h1>PAGE NO FOUND !!!</h1>} />
